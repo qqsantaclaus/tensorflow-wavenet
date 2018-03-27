6d881e1f220bc4bb5cfd4955f42c45a31327fa5a
Jiaqi Su <jiaqisu@jiaqis-imac.CS.Princeton.EDU>
2018-03-27 14:42:26 -0400

    new tr scripts

--- tr_scripts/all_speakers.sh
+++ tr_scripts/all_speakers.sh
@@ -1,2 +1,2 @@
-# all_speakers, gc & lc enabled, 100000 iterations
+# All speakers, gc & lc enabled, 100000 iterations, wavenet model using padding
 python train.py --data_dir="../VCTK-Corpus" --gc_channels=32 --lc_channels=20 --lc_maps_json="../VCTK-Corpus/maps.json" --logdir="./logdir/train/2018-03-12T20-32-44" --histograms=True --learning_rate=0.0002 --batch_size=4
--- tr_scripts/p225.sh
+++ tr_scripts/p225.sh
@@ -1,2 +1,2 @@
-# Training with Single Speaker 255
+# Training with Single Speaker 225, only lc enabled, 100000 iterations, wavenet model using padding on lc
 python train.py --data_dir="../VCTK-Corpus/wav48/p225" --lc_channels=20 --lc_maps_json="../VCTK-Corpus/wav48/p225/maps_p225.json" --logdir="./logdir/train/p225" --learning_rate=0.00002 --batch_size=2
